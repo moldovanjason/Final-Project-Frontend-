6bd3c590b62a53f1e01e04a219a161b9606551c9
moldovanjason <moldovanjason@gmail.com>
2020-08-25 20:22:10 +0000

    add functionality to editprofile.js

--- src/js/views/editProfile.js
+++ src/js/views/editProfile.js
@@ -1,10 +1,114 @@
 import React from "react";
+import PropsTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 //create your first component
-export function EditProfile(props) {
-	return (
-		<div className="text-center mt-5">
-			<h1>edit profile</h1>
-		</div>
-	);
+export class EditProfile extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fullName: null,
+			email: null,
+			password: null
+		};
+	}
+
+	// editContact = (name, email, id) => {
+	// 			fetch(`address`, {
+	// 				method: "PUT",
+	// 				headers: {
+	// 					"Content-Type": "application/json"
+	// 				},
+	// 				body: JSON.stringify({
+	// 					agenda_slug: "expAgendaForCohortIII",
+	// 					full_name: name,
+	// 					email
+	// 				})
+	// 			})
+	// 				.then(data => data.json())
+	// 				.then(({ status, resMsg }) => {
+	// 					if (status === 400) alert(resMsg);
+	// 				})
+	// 				.then(() => {
+	// 					fetch("address")
+	// 						.then(res => res.json())
+	// 						.then(data => setStore({ allContacts: data }));
+	// 				})
+	// 				.catch(err => alert(err.message));
+	// 		}
+
+	render() {
+		return (
+			<div>
+				<h1>edit profile</h1>
+				<div className="navbar navbar-expand-lg ">
+					<a className="papertrade navbar-brand" href="">
+						PAPER TRADE
+					</a>
+					<a className="signup nav-link" href="#" onClick={() => props.history.push("/signup")}>
+						Sign Up
+					</a>
+				</div>
+				<div>
+					<h1 className="text-center mt-5">Edit Profile</h1>
+					<form>
+						<div className="form-group">
+							<label>Full Name</label>
+							<input
+								onChange={event => {
+									this.setState({ ...state, full_name: event.target.value });
+								}}
+								type="text"
+								className="form-control"
+								placeholder="Enter New Full Name"
+								value={this.state.full_name}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Email</label>
+							<input
+								onChange={event => {
+									this.setState({ ...state, email: event.target.value });
+								}}
+								type="email"
+								className="form-control"
+								placeholder="Enter New Email"
+								value={this.state.email}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Password</label>
+							<input
+								onChange={event => {
+									this.setState({ ...state, password: event.target.value });
+								}}
+								type="password"
+								className="form-control"
+								placeholder="Enter New Password"
+								value={this.state.password}
+							/>
+						</div>
+						<button
+							// disabled={disabledButton}
+							onClick={() => {
+								this.editContact(state.fullName, state.email, props.match.params.id);
+								props.history.push("/profile");
+							}}
+							type="button"
+							// className={`btn ${disabledButton ? "btn-secondary" : "btn-primary"} form-control`}
+						>
+							save
+						</button>
+						<Link className="mt-3 w-100 text-center" to="/profile">
+							or get back to profile
+						</Link>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
+
+EditProfile.propTypes = {
+	history: PropsTypes.object
+};
--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -4,14 +4,59 @@ import PropsTypes from "prop-types";
 //create your first component
 export function Profile(props) {
 	return (
-		<div className="navbar navbar-expand-lg ">
-			<a className="papertrade navbar-brand" href="" onClick={() => props.history.push("/profile")}>
-				PAPER TRADE
-			</a>
-			<div className="vlprofile" />
-			<a className="signup nav-link" href="" onClick={() => props.history.push("/home")}>
-				Log Out
-			</a>
+		<div>
+			<div className="profilenavbar navbar-expand-lg ">
+				<a className="papertrade navbar-brand" href="" onClick={() => props.history.push("/profile")}>
+					PAPER TRADE
+				</a>
+				<div className="vlprofile" />
+				<a className="editprofile nav-link" href="" onClick={() => props.history.push("/editprofile")}>
+					Edit Profile
+				</a>
+				<a className="logout nav-link" href="" onClick={() => props.history.push("/home")}>
+					Log Out
+				</a>
+			</div>
+			<div>Jason Moldovn</div>
+			<div>Net Paper Acount Value:10000</div>
+			<div>Buying Power:10000</div>
+			<div>
+				<div>Buy Stocks</div>
+				<div>Transaction History</div>
+				<div>Leader Board</div>
+			</div>
+			<div>MY STOCKS</div>
+			<div>
+				<table className="table table-borderless">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">First</th>
+							<th scope="col">Last</th>
+							<th scope="col">Handle</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Jacob</td>
+							<td>Thornton</td>
+							<td>@fat</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td colSpan="2">Larry the Bird</td>
+							<td>@twitter</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
