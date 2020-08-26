bc5bbee328ea0d393d69068e756c41757a770a4c
Gerdeth <gerdethwickham@gmail.com>
2020-08-26 21:53:13 +0000

    stock update

--- src/js/views/stockPage.js
+++ src/js/views/stockPage.js
@@ -4,7 +4,33 @@ import React from "react";
 export function StockPage(props) {
 	return (
 		<div className="text-center mt-5">
-			<h1>Stock page</h1>
+			<h1>Stock Page</h1>
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th scope="col">Symbol</th>
+						<th scope="col">Name</th>
+						<th scope="col">Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">APPL</th>
+						<td>Apple</td>
+						<td>$500.10</td>
+					</tr>
+					<tr>
+						<th scope="row">F</th>
+						<td>Ford</td>
+						<td>$7.20</td>
+					</tr>
+					<tr>
+						<th scope="row">XSPA</th>
+						<td>X-Spa</td>
+						<td>$3.00</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	);
 }
