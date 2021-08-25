f5c2aaac5b2307db29a194530e16e4057f04ec4f
moldovanjason <moldovanjason@gmail.com>
2021-08-25 18:34:06 +0000

    deleting comments

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -36,38 +36,6 @@ export const Navbar = props => {
 				</Link>
 			</div>
 		</nav>
-
-		// <nav className="navbar navbar-expand-lg">
-		// 	<div className="collapse navbar-collapse" id="navbarNav">
-		// 		<ul className="navbartext navbar-nav">
-		// 			<li className="nav-item">
-		// 				<Link className="tradestocks nav-link" to="/tradestocks">
-		// 					Trade Stocks
-		// 				</Link>
-		// 			</li>
-		// 			<li className="nav-item">
-		// 				<Link className="transactionhistory nav-link" to="/transactionhistory">
-		// 					Transaction History
-		// 				</Link>
-		// 			</li>
-		// 			<li className="nav-item">
-		// 				<Link className="leaderboard nav-link" to="/leaderboard">
-		// 					Leader Board
-		// 				</Link>
-		// 			</li>
-		// 			<li className="nav-item">
-		// 				<Link className="mystocks nav-link" to="/mystocks">
-		// 					My Stocks
-		// 				</Link>
-		// 			</li>
-		// 			<li className="nav-item">
-		// 				<Link className="userprofile nav-link" to="/userprofile">
-		// 					User Profile
-		// 				</Link>
-		// 			</li>
-		// 		</ul>
-		// 	</div>
-		// </nav>
 	);
 };
 Navbar.propTypes = {
@@ -75,20 +43,3 @@ Navbar.propTypes = {
 };
 
 export default withRouter(Navbar);
-
-{
-	/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <Link class="navbar-brand" to="#">Navbar</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <Link class="nav-link active" to="#">Home <span class="sr-only">(current)</span></Link>
-      <Link class="nav-link" to="#">Features</Link>
-      <Link class="nav-link" to="#">Pricing</Link>
-      <Link class="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
-    </div>
-  </div>
-</nav> */
-}
--- src/js/component/signUpModal.js
+++ src/js/component/signUpModal.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
-// import "../../styles/signUpModal.scss";
 import { Context } from "../store/appContext";
 
 export const SignUpModal = props => {
